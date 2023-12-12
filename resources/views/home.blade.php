b1451b316d2187be3694374658fcf4e6bfad396a
AlexAaqil <alexaaqil.se@gmail.com>
2023-12-12 12:36:57 +0300

    Added assets in the public folder for styles and images. Added partials for the layout to be extended and nav and footer. Added some basic code for the homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,13 @@
-<h1>Home</h1>
+@extends('partials.base')
+
+@section('content')
+
+<main class="Homepage">
+    <section class="Hero">
+        <div class="container">
+            <h1>Home</h1>
+        </div>
+    </section>
+</main>
+
+@endsection
