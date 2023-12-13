bcbdcec9b9ec54ab5bd41cd9fac2ee834572a5a0
lawrencemwangi <lawrencemwangingige@gmail.com>
2023-12-13 15:29:52 +0300

    Updated the content and styles for both Product and Categories Views

--- resources/views/admin/categories.blade.php
+++ resources/views/admin/categories.blade.php
@@ -5,6 +5,10 @@
     @include('admin.aside')
     <section class="main_content Categories">
         <h1>Categories</h1>
+        <input type="text" name="search" id="search" placeholder="Search" />
+        <div class="btn">
+            <button>Add New</button>
+        </div>
     </section>
 </main>
 @endsection
