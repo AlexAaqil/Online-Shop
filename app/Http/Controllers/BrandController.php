4010e91ff4e1b00cfd734e61c11b6ec563ba8bc4
alexaaqil <alexaaqil.se@gmail.com>
2023-12-18 16:59:25 +0300

    Added the logic for listing, adding , updating and deleting the Brands

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -1,10 +1,56 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Brand;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class BrandController extends Controller
 {
-    //
+    public function list_brands() {
+        $brands = Brand::all();
+        return view("admin/brands", compact("brands"));
+    }
+
+    public function get_add_brand() {
+        return view("admin/add_brand");
+    }
+
+    public function post_add_brand(Request $request) {
+        request()->validate([
+            'title'=> 'required|unique:categories',
+        ]);
+
+        $brand = new Brand;
+        $brand->title = $request->title;
+        $brand->slug = Str::slug( $request->title);
+        $brand->save();
+
+        return redirect()->route('list_brands')->with('success', "Brand was added successfully");
+    }
+
+    public function get_update_brand($id) {
+        $brand = Brand::find($id);
+        return view("admin/update_brand", compact('brand'));
+    }
+
+    public function post_update_brand($id, Request $request) {
+        request()->validate([
+            'title' => 'required|unique:categories,title,'.$id,
+        ]);
+
+        $brand = Brand::find($id);
+        $brand->title = $request->title;
+        $brand->slug = Str::slug( $request->title );
+        $brand->save();
+
+        return redirect()->route('list_brands')->with('success', "Brand was updated successfully");
+    }
+
+    public function delete_category($id) {
+        $brand = Brand::find($id);
+        $brand->delete();
+
+        return redirect()->route('list_brands')->with('success', "Brand deleted successfully!");
+    }
 }
