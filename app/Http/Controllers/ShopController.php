263d5e3ac938fd2818ebb769a0f8e90b8801dca0
alexaaqil <alexaaqil.se@gmail.com>
2023-12-21 16:08:15 +0300

    Added categorised_products view for showing products related to a specified category either through the sidenav filters or by just using the url

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
     public function shop() {
         $products = Product::all();
-        return view("shop", compact('products'));
+        $categories = Category::all();
+        return view("shop", compact('products', 'categories'));
+    }
+
+    public function categorised_products($slug) {
+        $products = Product::whereHas('category', function ($query) use ($slug) {
+            $query->where('slug', $slug);
+        })->get();
+        $category = Category::where('slug', $slug)->first();
+        $categories = Category::all();
+        return view("categorised_products", compact('products', 'category', 'categories'));
     }
 }
--- resources/views/categorised_products.blade.php
+++ resources/views/categorised_products.blade.php
@@ -4,12 +4,17 @@
 @include('partials.navbar')
 
 <main class="Shop">
+    <div class="hero">
+        <h1>{{ $category->title }}</h1>
+    </div>
+
     <div class="topnav">
         @include('partials.searchbar')
     </div>
 
     <div class="container">
-        @include('partials.shop_sidenav')
+         @include('partials.shop_sidenav')
+
         @include('partials.list_products')
     </div>
 </main>
